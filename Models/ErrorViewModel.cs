155cdbbc11e992a10e1e2e5645ac2cd9cf8812a3
FabioHenrique023 <fabiohomeoffice904@gmail.com>
2025-08-21 15:08:26 -0300

    configurando o projeto inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sistema_monitoramento_urbano.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
